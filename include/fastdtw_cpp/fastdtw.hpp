5e415626078cba3ee9e7c93a03635c823899890e
Richard Hanten <richard.hanten@uni-tuebingen.de>
2014-05-01 22:47:15 +0200

    preparing fast_dtw

--- include/fastdtw_cpp/fastdtw.hpp
+++ include/fastdtw_cpp/fastdtw.hpp
@@ -6,65 +6,53 @@
 
 namespace fastdtw_cpp {
 namespace fastdtw {
+/// non-recursive
 template<typename T>
-void arr(const std::vector<T> &signal_a, const std::vector<T> &signal_b,
+void pyr(const std::vector<T> &signal_a, const std::vector<T> &signal_b,
          const unsigned int radius,
-         path::WarpPath<T> &path)
+         path::WarpPath path)
 {
-    arr_rec(signal_a, signal_b, radius, 2,  path);
+    /// step 1: build the pyramid
+    /// step 2: eval dtw for highest level
+    /// step 3: project path from level to level
+    /// step 4: only evaluate projected cells of path in lower resolution
 }
 
+namespace {
 template<typename T>
-void arr_rec(const std::vector<T> &signal_a, const std::vector<T> &signal_b,
-             const unsigned int radius, const unsigned int shrink_fac,
-             path::WarpPath<T> &path)
+void rec_step(const std::vector<T> &signal_a, const std::vector<T> &signal_b,
+              const unsigned int radius, const unsigned int shrink_level,
+              path::WarpPath<T> &path)
 {
     assert(signal_a.size() != 0);
     assert(signal_b.size() != 0);
     /// signal_a is rows
     /// signal_b is cols
-
     unsigned int min_coarsest_res = radius + 2;
+    unsigned int size_a = signal_a.size();
+    unsigned int size_b = signal_b.size();
 
-    if(signal_a.size() < min_coarsest_res || signal_b.size() < min_coarsest_res) {
+    if(size_a < min_coarsest_res || size_b < min_coarsest_res) {
         dtw::std(signal_a, signal_b, path);
     } else {
-        std::vector<T> shrunk_a;
-        std::vector<T> shrunk_b;
-        utils::shrink_static<T,2>(signal_a, shrunk_a);
-        utils::shrink_static<T,2>(signal_b, shrunk_b);
+        std::vector<T> shrunk_a, shrunk_b;
+        utils::shrink_static<T>(signal_a, shrunk_a);
+        utils::shrink_static<T>(signal_b, shrunk_b);
         path::WarpPath<T> sub_path;
-        fastdtw::arr(shrunk_a, shrunk_b, radius, sub_path);
-        window(signal_a, signal_b, shrunk_a, shrunk_b, shrink_fac, sub_path);
-
-
-
-//        final SearchWindow window = new ExpandedResWindow(tsI, tsJ, shrunkI, shrunkJ,
-//                                                          FastDTW.getWarpPathBetween(shrunkI, shrunkJ, searchRadius, distFn),
-//                                                          searchRadius);
-
-
-        /// shrink
+        rec_step(shrunk_a, shrunk_b, radius, shrink_level * 2, sub_path);
+        /// project resulting path and make new search window
+        /// search by evaluating the projected cells
     }
+
+}
 }
 
-
-template<typename T>
-void window(const std::vector<T> &signal_a, const std::vector<T> &signal_b,
-            const std::vector<T> &shrunk_a, const std::vector<T> &shrunk_b,
-            const unsigned int shrink_fac,
-            const path::WarpPath<T> &path)
+/// recursive
+void rec(const std::vector<T> &signal_a, const std::vector<T> &signal_b,
+         const unsigned int radius,
+         path::WarpPath<T> &path)
 {
-
-    unsigned int size = path.size();
-    for(unsigned int i = 0 ; i < size ; ++i) {
-        unsigned int x = path.x(i);
-        unsigned int y = path.y(i);
-
-
-    }
-
-
+    rec_step(signal_a, signal_b, radius, 2, path);
 }
 }
 }
---
+++ include/fastdtw_cpp/projection.hpp
@@ -0,0 +1,9 @@
+#ifndef PROJECTION_HPP
+#define PROJECTION_HPP
+namespace fastdtw_cpp {
+namespace projection {
+
+}
+}
+
+#endif // PROJECTION_HPP
