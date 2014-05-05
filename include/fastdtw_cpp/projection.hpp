cb14b94b3bd3a6e7fe1f7f36fdd8d058083a09ed
Richard Hanten <richard.hanten@uni-tuebingen.de>
2014-05-05 11:39:12 +0200

    radius enabled

--- include/fastdtw_cpp/projection.hpp
+++ include/fastdtw_cpp/projection.hpp
@@ -5,8 +5,18 @@
 
 namespace fastdtw_cpp {
 namespace projection {
+/**
+ * @brief The Projection class can be used to project a warp path
+ *        into a higher resolution space.
+ */
 class Projection {
 public:
+    /**
+     * @brief Projection constructor.
+     * @param _height       height of the lower resolution space
+     * @param _width        width of the lower resolution space
+     * @param _scale        the upscale of the lower resolution
+     */
     Projection(const unsigned int _height,
                const unsigned int _width,
                const unsigned int _scale) :
@@ -17,8 +27,14 @@ public:
     {
     }
 
+    /**
+     * @brief Project a warp path to the higher resolution space.
+     * @param path      the path to project
+     * @param radius    additional radius to enlarge projection area
+     */
     template<typename T>
-    inline void project(const path::WarpPath<T> &path)
+    inline void project(const path::WarpPath<T> &path,
+                        const unsigned int radius = 0)
     {
         unsigned int curr_it = 0;
         unsigned int next_it = 1;
@@ -34,33 +50,40 @@ public:
 
             if(next_x > curr_x && next_y > curr_y ){
                 if(was_dia)
-                    diagonal(curr_x, curr_y);
+                    diagonal(curr_x, curr_y, 0, radius);
                 else {
-                    diagonalStart(curr_x, curr_y);
+                    diagonalStart(curr_x, curr_y, radius);
                     was_dia = true;
                 }
             } else {
-                block(curr_x, curr_y);
+                block(curr_x, curr_y, radius);
                 was_dia = false;
             }
-
-            block(next_x, next_y);
             ++curr_it;
             ++next_it;
         }
+        unsigned int last = path.size() - 1;
+        block(x_ptr[last], y_ptr[last], radius);
     }
 
+    /**
+     * @brief Project a block in to the higher resolution space.
+     * @param x         the block x coordinate in lower resolution space
+     * @param y         the block y coordinate in lower resolution space
+     * @param radius    additional radius to enlarge projection area
+     */
     inline void block(const unsigned int x,
-                      const unsigned int y)
+                      const unsigned int y,
+                      const unsigned int radius = 0)
     {
-        unsigned int anchor_x = x * scale_;
-        unsigned int anchor_y = y * scale_;
+        unsigned int anchor_x = std::max(0, (int)(x * scale_) - (int)radius);
+        unsigned int anchor_y = std::max(0, (int)(y * scale_) - (int)radius);
 
         assert(anchor_x < width_);
         assert(anchor_y < height_);
 
-        unsigned int max_x = std::min(anchor_x + scale_, width_);
-        unsigned int max_y = std::min(anchor_y + scale_, height_);
+        unsigned int max_x = std::min(anchor_x + scale_ + radius, width_);
+        unsigned int max_y = std::min(anchor_y + scale_ + radius, height_);
 
         for(unsigned int it_y = anchor_y ; it_y < max_y ; ++it_y) {
             unsigned int pos = it_y * width_;
@@ -70,10 +93,16 @@ public:
         }
     }
 
-
+    /**
+     * @brief Project a diagonal elemnt in the higher resoluation space.
+     * @param x         lower resolution x coordinate of the anchor
+     * @param y         lower resolution y coordinate of the anchor
+     * @param dia_off   the diagonal offset - elements which are left out
+     */
     inline void diagonal(const unsigned int x,
                          const unsigned int y,
-                         const unsigned int dia_off = 0)
+                         const unsigned int dia_off = 0,
+                         const unsigned int radius = 0)
     {
         unsigned int anchor_x = x * scale_;
         unsigned int anchor_y = y * scale_;
@@ -82,50 +111,79 @@ public:
         assert(anchor_y < height_);
 
         for(unsigned int dia = dia_off ; dia < scale_ ; ++dia) {
-            corner(anchor_x + dia, anchor_y + dia);
+            corner(anchor_x + dia, anchor_y + dia, radius);
         }
 
     }
 
+    /**
+     * @brief Spawn a diagonal starting element.
+     * @param x         lower resolution x coordinate of the anchor
+     * @param y         lower resolution y coordinate of the anchor
+     * @param radius    additional radius to enlarge projection area
+     */
     inline void diagonalStart(const unsigned int x,
-                              const unsigned int y)
+                              const unsigned int y,
+                              const unsigned int radius = 0)
     {
-        block(x, y);
+        block(x, y, radius);
         diagonal(x, y, 1);
     }
 
+    /**
+     * @brief Return the projection value of a certaing point.
+     * @param x     high resolution x coordinate
+     * @param y     high resolution y coordinate
+     * @return      true for projected path element
+     */
     inline bool at(const unsigned int x, const unsigned int y) const {
         return data_.at(y * width_ + x);
     }
 
+    /**
+     * @brief   Return the width of the projection space.
+     * @return      the width of the higher resolution space
+     */
     inline unsigned int width() const
     {
         return width_;
     }
 
+    /**
+     * @brief   Return the height of the projection space.
+     * @return      the height of the higher resolution space
+     */
     inline unsigned int height() const
     {
         return height_;
     }
 
 private:
+    /**
+     * @brief Project a corner element.
+     * @param x     the higher resolution anchor x coordinate
+     * @param y     the higher resolution anchor y coordinate
+     */
     inline void corner(const unsigned int x,
-                       const unsigned int y)
+                       const unsigned int y,
+                       const unsigned int radius = 0)
     {
 
-        unsigned int off = scale_ - 1;
-        unsigned int anchor_y = y + off;
-        unsigned int anchor_x = x + off;
+        unsigned int off   = scale_ - 1;
+        unsigned int off_y = y + off;
+        unsigned int off_x = x + off;
+        unsigned int min_x = std::max(0, (int) x - (int) radius);
+        unsigned int min_y = std::max(0, (int) y - (int) radius);
         unsigned int max_y = std::min(height_, y + scale_ - 1);
         unsigned int max_x = std::min(width_, x + scale_);
 
-        if(anchor_x < width_)
-            for(unsigned it_y = y ; it_y < max_y; ++it_y)
-                data_.at(it_y * width_ + anchor_x) = true;
+        if(off_x < width_)
+            for(unsigned it_y = min_y ; it_y < max_y; ++it_y)
+                data_.at(it_y * width_ + off_x) = true;
 
-        if(anchor_y < height_) {
-            unsigned int pos = anchor_y * width_;
-            for(unsigned it_x = x ; it_x < max_x; ++it_x)
+        if(off_y < height_) {
+            unsigned int pos = off_y * width_;
+            for(unsigned it_x = min_x ; it_x < max_x; ++it_x)
                 data_.at(pos + it_x) = true;
         }
     }
--- src/opencv.cpp
+++ src/opencv.cpp
@@ -35,105 +35,6 @@ void project_static(const fastdtw_cpp::path::WarpPath<T> &src,
         }
     }
 }
-
-/// GO STRAIGHT
-//void block(const unsigned int x,
-//           const unsigned int y,
-//           const unsigned int width,
-//           const unsigned int height,
-//           const unsigned int size,
-//           bool *projection)
-//{
-//    unsigned int max_x = std::min(x + size, width);
-//    unsigned int max_y = std::min(y + size, height);
-
-//    for(unsigned int it_y = y ; it_y < max_y ; ++y) {
-//        unsigned int pos = y * width;
-//        for(unsigned int it_x = x ; it_x < max_x ; ++x) {
-//            projection[pos + x] = true;
-//        }
-//    }
-//}
-
-//inline void corner(const unsigned int x,
-//                   const unsigned int y,
-//                   const unsigned width,
-//                   const unsigned height,
-//                   const unsigned size,
-//                   bool *projection)
-//{
-//    unsigned int off = size - 1;
-//    unsigned int max_x = std::min(x + off, width);
-//    unsigned int max_y = std::min(y + size, height);
-//    unsigned int pos_x = max_x - 1;
-//    unsigned int pos_y = (max_y - 1) * width;
-
-//    for(unsigned int it_y = y ; it_y < max_y ; ++it_y) {
-//        projection[it_y * width + pos_x] = true;
-//    }
-//    for(unsigned int it_x = x ; it_x < max_x ; ++it_x) {
-//        projection[posy + x] = true;
-//    }
-//}
-
-//void diagonal(const unsigned int x,
-//              const unsigned int y,
-//              const unsigned size,
-//              fastdtw_cpp::projection::Projection &projection)
-//{
-//    unsigned int max_x = std::min(x + size, width);
-//    unsigned int max_y = std::min(y + size, height);
-
-//    for(unsigned int dia = 0 ; dia < size ; ++dia) {
-//        unsigned int dia_x = dia + x;
-//        unsigned int dia_y = dia + y;
-//        corner(dia_x, dia_y, width, height, size, projection);
-//    }
-
-//    for(unsigned int it_y = y ; it_y < max_y ; ++y) {
-//        unsigned int pos = y * width;
-//        for(unsigned int it_x = x ; it_x < max_x ; ++x) {
-//            projection[pos + x] = true;
-//        }
-//    }
-//}
-
-
-//template<typename T>
-//void project(const fastdtw_cpp::path::WarpPath &src,
-//             fastdtw_cpp::path::WarpPath<T> &dst,
-//             const unsigned int scale = 2,
-//             const unsigned int radius = 0)
-//{
-//    assert(dst.empty());
-//    unsigned int size = src.size();
-//    unsigned int offs = scale - 1;
-//    unsigned int last = size - 1;
-//    unsigned int  width  = x_ptr[last] + offs;
-//    unsigned int  height = y_ptr[last] + offs;
-//    fastdtw_cpp::projection::Projection(height, width);
-//    bool *projection_ptr = projection.data();
-//    const unsigned int *x_ptr = src.x_ptr();
-//    const unsigned int *y_ptr = src.y_ptr();
-
-//    unsigned int curr_it = 0;
-//    unsigned int next_it = 1;
-//    while(next_it < size) {
-//        unsigned int curr_x = x_ptr[curr_it];
-//        unsigned int next_x = x_ptr[next_it];
-//        if(next_x > curr_x) {
-//            diagonal(curr_x * scale, y_ptr[curr_it] * scale,
-//                     width, height, size, projection_ptr);
-//        } else {
-//            block(curr_x * scale, y_ptr[curr_x] * scale,
-//                  width, height, scale, projection_ptr);
-//        }
-//        ++curr_it;
-//        ++next_it;
-//    }
-//    block(x_ptr[next_it] * scale, y_ptr[next_it] * scale,
-//          width, height, scale, projection);
-//}
 }
 
 
@@ -145,15 +46,19 @@ int main(int argc, char *argv[])
     images.push_back(cv::Mat(40,40, CV_8UC1, cv::Scalar::all(0)));
 
     fastdtw_cpp::path::WarpPath<float> path;
-    path.push_back(0,0);
-    path.push_back(0,1);
-    for(unsigned int i = 2 ; i < 19 ; ++i)
-        path.push_back(i-1,i);
-    path.push_back(18,19);
-    path.push_back(19,19);
+//    path.push_back(0,0);
+//    path.push_back(0,1);
+//    for(unsigned int i = 2 ; i < 19 ; ++i)
+//        path.push_back(i-1,i);
+//    path.push_back(18,19);
+//    path.push_back(19,19);
+        for(unsigned int i = 0 ; i < 20 ; ++i)
+            path.push_back(i,i);
+
+
 
     fastdtw_cpp::projection::Projection proj(1 + path.max_x(), 1 + path.max_y(), 2);
-    proj.project(path);
+    proj.project(path, 0);
 
     fastdtw_cpp::path::WarpPath<float> projected_path;
     project_static<float,2>(path, projected_path);
