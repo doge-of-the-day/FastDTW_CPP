b21a3921f672bb434aaf5eec77cdb3fc5f1dc9ed
Richard Hanten <Richard.Hanten@uni-tuebingen.de>
2016-12-15 15:45:36 +0100

    restruct

--- include/fastdtw_cpp/downsampling.h
+++ include/fastdtw_cpp/downsampling.h
@@ -7,6 +7,12 @@ namespace downsampling {
 #define FILTER_TYPE_BOX      0
 #define FILTER_TYPE_BINOMIAL 1
 
+/**
+ * @brief Downsample a signal sequence of given length.
+ * @param source        - the original signal
+ * @param source_length - the lenght of the signal
+ * @param target        - the downsampled version
+ */
 template <typename base_type, unsigned int scale, unsigned int filter_type, unsigned int filter_size>
 void downSample(const              base_type* source,
                 const unsigned int source_length,
